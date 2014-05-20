b8dd532f6e5be91d2b1407aabb2849dd2058de45
Alexander Scott <xander@cadcoder.com>
2014-05-20 11:33:12 +1000

    XCode New Project

---
+++ CoreDataDemo/CoreDataDemo/CCAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CCAppDelegate.h
+//  CoreDataDemo
+//
+//  Created by Alexander Scott on 20/05/2014.
+//  Copyright (c) 2014 CADCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CoreDataDemo/CoreDataDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataDemo
+//
+//  Created by Alexander Scott on 20/05/2014.
+//  Copyright (c) 2014 CADCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
